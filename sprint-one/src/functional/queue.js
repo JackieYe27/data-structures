3c69b5d9f34e767335ec0734c84aa828802c79d5
Jackie Ye <jackieye@Jackies-MacBook-Pro.local>
2019-04-10 21:42:22 -0700

    Completed Functional for Stack and Queue.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,6 @@
 var Queue = function() {
   var someInstance = {};
+  var count = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +8,23 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    count++;
+    storage[count] = value;
   };
 
   someInstance.dequeue = function() {
+    if (count === 0) {
+      return undefined;
+    }
+    var first = Object.keys(storage).shift() // value
+    var deleted = storage[first]
+    delete storage[first];
+    count--
+    return deleted;  
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,40 @@
 var Stack = function() {
   var someInstance = {};
+  var count = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
-
   // Implement the methods below
+
+  // Add the given value into storage w/ a key of current count 
   someInstance.push = function(value) {
+    count ++;
+    storage[count] = value;
   };
+ 
+  /* Check to see if stack is empty
+    if so, return undefined
+  Decrement count
+  Save element at top of stack to a var (to later return)
+  Delete that element from storage
+  Return saved element
+  */
 
   someInstance.pop = function() {
+    if (count === 0) {
+      return undefined;
+    }
+     var deleted = storage[count]
+     delete storage[count];
+     count--
+     return deleted;
   };
 
+
+  // return count;
+
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
